cd875413eed8d643271f490262bfa910f73ebd7d
irishcajun75 <irishcajun75@gmail.com>
2026-07-20 12:54:47 -0500

    completed reverse string

--- day-1/homework/reverse-string.js
+++ day-1/homework/reverse-string.js
@@ -13,15 +13,37 @@
  *
  * TIPS:
  *  a. create something to collect the output you'll return.
+ *      
+ * 
  *  b. you'll need a loop, which one is best? How do you know
- *     when to stop looping?
- *  c. how do you access individual characters of a String?
- *  d. how do you concatenate Strings? What operator do we use?
+ *     when to stop looping? " Since I know the exact length of the string, a standard
+ *      for loop is perfect here.  To reverse a string, you start at the last index and step
+ *        backward down to index 0"
+ * 
+ *  c. how do you access individual characters of a String? "You can access characters in a string using
+ *      bracket notation [], just like an array"
+ * 
+ *  d. how do you concatenate Strings? What operator do we use? "You use the + operator or += to attach
+ *      characters onto your collector variable"
+ * 
  */
 
 function reverseString(input) {
   // YOUR CODE HERE //
+  // Storage for the reversed string.
+  let reversed = "";
+
+  // Loop backward starting from the last characterdown to index 0
+  for (let i = input.length - 1; i>= 0; i--) {
+    // Access character and concatente it onto our output
+    reversed += input[i];
+  }
+
+  // Return the completed reversed string
+  return reversed;
 }
+
+console.log(reverseString("hello"));
 
 // DON'T REMOVE THIS CODE //////////////////////////////////////////////////////
 if (
